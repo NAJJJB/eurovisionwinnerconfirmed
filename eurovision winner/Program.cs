113849d02aa63fe926c7500c8b8b8910f3a54758
Neo <neo@najjjb.xyz>
2024-04-13 22:30:09 +0200

    Add files

---
+++ eurovision winner/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+
+namespace eurovision_winner
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string svar;
+            
+            Console.WriteLine("Please write who will win Eurovision 2024: ");
+            
+            svar = Console.ReadLine().ToUpper();
+            if (svar == "EUROPAPA")
+            {
+                Console.WriteLine("YES! 12 POINTS TO THE NETHERLANDS");
+            }
+            else
+            {
+                Console.WriteLine("You are wrong, I have now planted malware on your computer hahaha.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
